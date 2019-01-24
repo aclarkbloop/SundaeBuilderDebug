8f0561d0c865ab9f6a36f0c084a196a3962cc4fc
aclarkbloop <akclarknp@gmail.com>
2019-01-23 22:39:34 -0500

    added comments

--- src/sundae/takeOrder.java
+++ src/sundae/takeOrder.java
@@ -7,11 +7,16 @@ public class takeOrder {
 	public static void main(String[] args) throws Exception {
 		Scanner s = new Scanner(System.in);
 		
+		// Scanner s reads in user input from the console. 
+		
 		System.out.println("Welcome to the ice cream shop! How many customers are in your group?");
 		int numCustomers = s.nextInt();
 		double totalPrice = 0.0;
 		int totalTime = 0;
 		
+		/* loop for the number of customers specified by the user and ask each customer to pick a flavor
+		 of icecream and the number of scoops */
+	
 		for (int i = 1; i < numCustomers + 1 ; i++) {
 			System.out.println("Customer " + i + ", pick a flavor of ice cream.");
 			String flavor = s.next();
@@ -21,22 +26,23 @@ public class takeOrder {
 			
 			IceCream iceCream = new IceCream(flavor, numScoops);
 			
-			
+			/* ask each customer how many toppings and call makeToppingArray method
+			with the number of toppings specified */			
 			
 			System.out.println("Now, how many toppings would you like? Choose up to 5!");
 			Topping[] toppings = makeToppingArray(s, s.nextInt());
-			
+
+			// create new SundaeBuilerImpl with the topping array returned by makeToppingArray
 			SundaeBuilderImpl build = new SundaeBuilderImpl(iceCream, toppings);
 			
 			double price = build.calculatePrice(toppings);
 			double waitTime = build.calculateTime();
-			// bug here where incrementing is incorrect
 			totalPrice += price;
 			totalTime += waitTime;
-			
+			// print total price and wait time for each customer
 			System.out.println("Your total is $" + price + " and your wait time is " + waitTime );
 		}
-		
+		// print total price and wait time for the entire customer group
 		System.out.println("The total cost for your group was $" + totalPrice + " and your total"
 				+ " wait time was " + totalTime + " minutes.");
 		
